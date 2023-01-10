698acc12e387121b81bd692f50dd7094d71a6484
canyonwan <canyonwan@126.com>
2023-01-10 22:48:06 +0800

    chore: test push to dev branch#2

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -76,6 +76,7 @@ export const useUserStore = defineStore({
 
     // 获取用户信息
     GetInfo() {
+      // test
       const that = this;
       return new Promise((resolve, reject) => {
         getUserInfo()
