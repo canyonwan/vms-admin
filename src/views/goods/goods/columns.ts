40afde839cf90a4df297203c743f61599deb0a54
canyonwan <canyonwan@126.com>
2023-02-01 22:19:34 +0800

    feat: add goods options

---
+++ src/views/goods/goods/columns.ts
@@ -0,0 +1,22 @@
+export const columns = [
+  {
+    title: 'id',
+    key: 'id',
+    width: 100
+  },
+  {
+    title: '名称',
+    key: 'name',
+    width: 140
+  },
+  {
+    title: '详情',
+    key: 'description',
+    width: 140
+  },
+  {
+    title: '创建时间',
+    key: 'createdAt',
+    width: 140
+  }
+]
