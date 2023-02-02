4ad74af475c0a05d063172ff0e3fb2dca5a28846
canyonwan <canyonwan@126.com>
2023-02-02 22:42:30 +0800

    feat: new create permission

--- src/views/system/menu/CreateDrawer.vue
+++ src/views/system/menu/CreateDrawer.vue
@@ -1,32 +1,15 @@
 <template>
   <n-drawer v-model:show="isDrawer" :width="width" :placement="placement">
     <n-drawer-content :title="title" closable>
-      <n-form :model="formParams" :rules="rules" ref="formRef" label-placement="left" :label-width="100">
-        <n-form-item label="类型" path="type">
-          <span>{{ formParams.type === 1 ? '侧边栏菜单' : '' }}</span>
-        </n-form-item>
-        <n-form-item label="标题" path="label">
-          <n-input placeholder="请输入标题" v-model:value="formParams.label" />
-        </n-form-item>
-        <n-form-item label="副标题" path="subtitle">
-          <n-input placeholder="请输入副标题" v-model:value="formParams.subtitle" />
+      <n-form :model="formData" :rules="rules" ref="formRef" label-placement="left" :label-width="100">
+        <n-form-item label="名称" path="name">
+          <n-input placeholder="请输入名称" v-model:value="formData.name" />
         </n-form-item>
         <n-form-item label="路径" path="path">
-          <n-input placeholder="请输入路径" v-model:value="formParams.path" />
+          <n-input placeholder="请输入路径" v-model:value="formData.path" />
         </n-form-item>
-        <n-form-item label="打开方式" path="openType">
-          <n-radio-group v-model:value="formParams.openType" name="openType">
-            <n-space>
-              <n-radio :value="1">当前窗口</n-radio>
-              <n-radio :value="2">新窗口</n-radio>
-            </n-space>
-          </n-radio-group>
-        </n-form-item>
-        <n-form-item label="菜单权限" path="auth">
-          <n-input placeholder="请输入权限，多个权限用，分割" v-model:value="formParams.auth" />
-        </n-form-item>
-        <n-form-item label="隐藏侧边栏" path="hidden">
-          <n-switch v-model:value="formParams.hidden" />
+        <n-form-item label="权限代码" path="code">
+          <n-input placeholder="请输入权限，多个权限用，分割" v-model:value="formData.code" />
         </n-form-item>
       </n-form>
 
@@ -41,13 +24,19 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, reactive, ref, toRefs } from 'vue'
   import { useMessage } from 'naive-ui'
+  import { savePermission } from '@/api/system/menu'
+  import type { IPermissionItem } from '@/api/system/types'
 
   const rules = {
-    label: {
+    name: {
       required: true,
-      message: '请输入标题',
+      message: '请输入权限名称',
+      trigger: 'change'
+    },
+    code: {
+      required: true,
+      message: '请输入权限代码',
       trigger: 'blur'
     },
     path: {
@@ -58,7 +47,6 @@
   }
   export default defineComponent({
     name: 'CreateDrawer',
-    components: {},
     props: {
       title: {
         type: String,
@@ -67,27 +55,34 @@
       width: {
         type: Number,
         default: 450
+      },
+      treeItem: {
+        type: Object as PropType<IPermissionItem>,
+        default: () => {}
       }
     },
-    setup() {
+    emits: ['handleOk'],
+    setup(props, { emit }) {
       const message = useMessage()
       const formRef: any = ref(null)
       const defaultValueRef = () => ({
-        label: '',
-        type: 1,
-        subtitle: '',
-        openType: 1,
-        auth: '',
+        name: '',
         path: '',
-        hidden: false
+        code: ''
       })
 
       const state = reactive({
         isDrawer: false,
         subLoading: false,
-        formParams: defaultValueRef(),
+        defaultForm: defaultValueRef(),
         placement: 'right',
         alertText: '该功能主要实时预览各种布局效果，更多完整配置在 projectSetting.ts 中设置，建议在生产环境关闭该布局预览功能。'
+      })
+      const formData = ref<IPermissionItem>({
+        parentId: 0,
+        name: '',
+        path: '',
+        code: ''
       })
 
       function openDrawer() {
@@ -99,11 +94,16 @@
       }
 
       function formSubmit() {
-        formRef.value.validate((errors) => {
+        formRef.value.validate(async (errors) => {
           if (!errors) {
+            // update
+            formData.value.id = props.treeItem && props.treeItem.id ? props.treeItem.id : undefined
+            formData.value.parentId = props.treeItem.parentId
+            await savePermission(formData.value)
             message.success('添加成功')
             handleReset()
             closeDrawer()
+            emit('handleOk')
           } else {
             message.error('请填写完整信息')
           }
@@ -112,11 +112,12 @@
 
       function handleReset() {
         formRef.value.restoreValidation()
-        state.formParams = Object.assign(state.formParams, defaultValueRef())
+        formData.value = Object.assign(formData.value, defaultValueRef())
       }
 
       return {
         ...toRefs(state),
+        formData,
         formRef,
         rules,
         formSubmit,
