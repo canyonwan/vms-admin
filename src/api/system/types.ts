bd729404a2030481c2a72b7b4e7154a269002014
canyonwan <canyonwan@126.com>
2023-02-02 22:41:36 +0800

    types: system types

--- src/api/system/types.ts
+++ src/api/system/types.ts
@@ -14,3 +14,13 @@ export interface IUserItem {
   updatedAt: string
   deletedAt?: string
 }
+
+export interface IPermissionItem {
+  id?: number
+  parentId: number
+  name: string
+  status?: number
+  code: string
+  path: string
+  children?: IPermissionItem[]
+}
