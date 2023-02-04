97fc6b452fdb044ddbb35359f68d35ddfc4b98d9
canyonwan <canyonwan@126.com>
2023-02-04 12:08:48 +0800

    feat: add role api

--- src/api/system/role.ts
+++ src/api/system/role.ts
@@ -1,11 +1,57 @@
 import { http } from '@/utils/http/axios'
+import type { IRoleItem } from './types'
+import { BasicPageInput } from '../types/common'
 
 /**
  * @description: 角色列表
  */
-export function getRoleList() {
+export function getRoleList(params: BasicPageInput): Promise<{ list: IRoleItem[] }> {
   return http.request({
-    url: '/role/list',
-    method: 'GET'
+    url: 'role',
+    method: 'GET',
+    params
+  })
+}
+
+/**
+ * @description: 删除角色
+ */
+export function deleteRole(id: number): Promise<void> {
+  return http.request({
+    url: `role/${id}`,
+    method: 'delete'
+  })
+}
+
+/**
+ * @description: 保存角色
+ */
+export function saveRole(params) {
+  return http.request({
+    url: 'role',
+    method: params.id ? 'put' : 'post',
+    data: params
+  })
+}
+
+/**
+ * @description: 保存角色权限
+ */
+export function bindRolePermissions(params): Promise<void> {
+  return http.request({
+    url: 'role/permission',
+    method: 'post',
+    data: params
+  })
+}
+
+/**
+ * @description: 移除角色权限
+ */
+export function removeRolePermissions(params): Promise<void> {
+  return http.request({
+    url: 'role/permission',
+    method: 'delete',
+    data: params
   })
 }
