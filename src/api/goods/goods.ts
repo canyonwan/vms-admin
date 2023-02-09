d55d1028a67a6b84dfb09356ee89eb50e63adb6b
canyonwan <canyonwan@126.com>
2023-02-09 23:14:01 +0800

    feat: add goods category api

--- src/api/goods/goods.ts
+++ src/api/goods/goods.ts
@@ -1,6 +1,6 @@
 import { http } from '@/utils/http/axios'
 import type { IResultPage } from '@/utils/http/axios/types'
-import type { IGoodsItem, IGoodsPageInput } from './types'
+import type { IGoodsCategoryTreeItem, IGoodsItem, IGoodsPageInput } from './types'
 
 /**
  * @description: 商品分页
@@ -8,6 +8,16 @@ import type { IGoodsItem, IGoodsPageInput } from './types'
 export function queryGoodsPage(params: any): Promise<IResultPage<IGoodsItem>> {
   return http.request({
     url: 'goods/page',
+    method: 'get',
+    params
+  })
+}
+/**
+ * @description: 商品分类分页
+ */
+export function queryGoodsCategoryPage(params: any): Promise<IResultPage<IGoodsCategoryTreeItem>> {
+  return http.request({
+    url: 'category/page',
     method: 'get',
     params
   })
@@ -31,5 +41,15 @@ export function saveGoods(params: IGoodsPageInput): Promise<void> {
     url: 'goods',
     method: params.id ? 'put' : 'post',
     data: params
+  })
+}
+
+/**
+ * @description: 商品分类
+ */
+export function queryGoodsCategoryTree(): Promise<{ list: IGoodsCategoryTreeItem[] }> {
+  return http.request({
+    url: 'category/tree',
+    method: 'get'
   })
 }
