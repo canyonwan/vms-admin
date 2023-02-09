13aa3990145a5ef0c44a43a2c56ebcb9e033b4e2
canyonwan <canyonwan@126.com>
2023-02-09 23:14:21 +0800

    feat: add goods category view

--- src/views/goods/category/index.vue
+++ src/views/goods/category/index.vue
@@ -1,5 +1,264 @@
 <template>
-  <div> 分类管理 </div>
+  <div>
+    <n-grid class="mt-4" cols="1 s:1 m:1 l:3 xl:3 2xl:3" responsive="screen" :x-gap="12">
+      <!-- 添加商品分类 -->
+      <n-gi span="1">
+        <n-card :segmented="{ content: true }" :bordered="false" size="small">
+          <template #header>
+            <n-space>
+              <n-dropdown trigger="hover" @select="selectAddMenu" :options="addMenuOptions">
+                <n-button type="info" ghost icon-placement="right">
+                  添加分类
+                  <template #icon>
+                    <div class="flex items-center">
+                      <n-icon size="14">
+                        <DownOutlined />
+                      </n-icon>
+                    </div>
+                  </template>
+                </n-button>
+              </n-dropdown>
+              <n-button type="info" ghost icon-placement="left" @click="packHandle">
+                全部{{ expandedKeys.length ? '收起' : '展开' }}
+                <template #icon>
+                  <div class="flex items-center">
+                    <n-icon size="14">
+                      <AlignLeftOutlined />
+                    </n-icon>
+                  </div>
+                </template>
+              </n-button>
+            </n-space>
+          </template>
+          <div class="w-full menu">
+            <n-input type="input" v-model:value="pattern" placeholder="输入商品分类名称搜索">
+              <template #suffix>
+                <n-icon size="18" class="cursor-pointer">
+                  <SearchOutlined />
+                </n-icon>
+              </template>
+            </n-input>
+            <div class="py-3 menu-list">
+              <template v-if="loading">
+                <div class="flex items-center justify-center py-4">
+                  <n-spin size="medium" />
+                </div>
+              </template>
+              <template v-else>
+                <n-tree
+                  label-field="categoryName"
+                  key-field="id"
+                  block-line
+                  cascade
+                  checkable
+                  :virtual-scroll="true"
+                  :pattern="pattern"
+                  :data="treeData"
+                  :expandedKeys="expandedKeys"
+                  style="max-height: 650px; overflow: hidden"
+                  @update:selected-keys="selectedTree"
+                  @update:expanded-keys="onExpandedKeys"
+                />
+              </template>
+            </div>
+          </div>
+        </n-card>
+      </n-gi>
+      <!-- 编辑商品分类 -->
+      <n-gi span="2">
+        <n-card :segmented="{ content: true }" :bordered="false" size="small">
+          <template #header>
+            <n-space>
+              <n-icon size="18">
+                <FormOutlined />
+              </n-icon>
+              <span>编辑商品分类{{ treeItem ? `：${treeItem.categoryName}` : '' }}</span>
+            </n-space>
+          </template>
+          <n-alert type="info" closable> 从商品分类列表选择一项后，进行编辑</n-alert>
+          <n-form :model="state.formData" :rules="rules" ref="formRef" label-placement="left" :label-width="100" v-if="isEditMenu" class="py-4">
+            <n-form-item label="类型" path="type">
+              <span>{{ '侧边栏商品分类' }}</span>
+            </n-form-item>
+            <n-form-item label="分类名称" path="categoryName">
+              <n-input placeholder="请输入名称" v-model:value="state.formData.categoryName" />
+            </n-form-item>
+            <n-form-item label="排序" path="sort">
+              <n-input placeholder="请输入排序" v-model:value="state.formData.sort" />
+            </n-form-item>
+            <n-form-item label="层级">
+              <n-input placeholder="请输入层级" v-model:value="state.formData.level" />
+            </n-form-item>
+            <n-form-item path="auth" style="margin-left: 100px">
+              <n-space>
+                <n-button type="primary" :loading="subLoading" @click="formSubmit">保存修改</n-button>
+                <n-button @click="handleReset">重置</n-button>
+                <n-button @click="handleDel">删除</n-button>
+              </n-space>
+            </n-form-item>
+          </n-form>
+        </n-card>
+      </n-gi>
+    </n-grid>
+    <!-- <CreateDrawer ref="createDrawerRef" :tree-item="treeItem" :title="drawerTitle" @handle-ok="onOk" /> -->
+  </div>
 </template>
+<script lang="ts" setup>
+  import { useDialog, useMessage } from 'naive-ui'
+  import { DownOutlined, AlignLeftOutlined, SearchOutlined, FormOutlined } from '@vicons/antd'
+  import { getTreeItem } from '@/utils'
+  // import CreateDrawer from './CreateDrawer.vue'
+  import { queryGoodsCategoryTree } from '@/api/goods/goods'
+  import type { IGoodsCategoryTreeItem } from '@/api/goods/types'
 
-<script setup lang="ts"></script>
+  const rules = {
+    categoryName: {
+      required: true,
+      message: '请输入商品分类名称',
+      trigger: 'change'
+    },
+    sort: {
+      required: true,
+      message: '请输入排序',
+      trigger: 'blur'
+    }
+  }
+
+  const formRef: any = ref(null)
+  const createDrawerRef = ref()
+  const message = useMessage()
+  const dialog = useDialog()
+
+  const treeItemKey = ref<number[]>([])
+
+  const expandedKeys = ref<number[]>([])
+
+  const treeData = ref<IGoodsCategoryTreeItem[]>([])
+
+  const defaultValue = () => ({
+    parentId: 0,
+    categoryName: '',
+    level: 0,
+    sort: 0
+  })
+
+  const loading = ref(true)
+  const subLoading = ref(false)
+  const isEditMenu = ref(false)
+  const treeItem = ref<IGoodsCategoryTreeItem>()
+  const pattern = ref('')
+  const drawerTitle = ref('')
+
+  const isAddSon = computed(() => {
+    return !treeItemKey.value.length
+  })
+
+  const addMenuOptions = ref([
+    {
+      label: '添加顶级商品分类',
+      key: 'home',
+      disabled: false
+    },
+    {
+      label: '添加子商品分类',
+      key: 'son',
+      disabled: isAddSon
+    }
+  ])
+
+  const state = reactive({
+    formData: defaultValue()
+  })
+
+  function selectAddMenu(key: string) {
+    // drawerTitle.value = key === 'home' ? '添加顶栏商品分类' : `添加子商品分类：${treeItem.value!.categoryName}`
+    openCreateDrawer()
+  }
+
+  function openCreateDrawer() {
+    const { openDrawer } = createDrawerRef.value
+    openDrawer()
+  }
+
+  function onOk() {
+    loadData()
+    // const { openDrawer } = createDrawerRef.value
+    // openDrawer()
+  }
+
+  function selectedTree(keys: number[]) {
+    if (keys.length) {
+      treeItem.value = getTreeItem(unref(treeData), keys[0])
+      treeItemKey.value = keys
+      // treeItemTitle.value = treeItem.value!.categoryName
+      Object.assign(state.formData, treeItem.value)
+      isEditMenu.value = true
+    } else {
+      isEditMenu.value = false
+      treeItemKey.value = []
+      treeItem.value = undefined
+      // treeItemTitle.value = ''
+    }
+  }
+
+  function handleDel() {
+    dialog.info({
+      title: '提示',
+      // content: `您确定想删除[${treeItem.value!.categoryName}]权限吗?`,
+      positiveText: '确定',
+      negativeText: '取消',
+      onPositiveClick: async () => {
+        // await deletePermission(treeItem.value!.id!)
+        loadData()
+        isEditMenu.value = false
+        treeItemKey.value = []
+        treeItem.value = undefined
+        // treeItemTitle.value = ''
+        // treeItem.value!.id! = 0
+        // treeItemId.value = 0
+        message.success('删除成功')
+      }
+    })
+  }
+
+  function handleReset() {
+    formRef.value.restoreValidation()
+    Object.assign(state.formData, defaultValue())
+  }
+
+  function formSubmit() {
+    formRef.value.validate(async (errors: any) => {
+      if (!errors) {
+        // await savePermission(state.formData)
+        message.success('修改成功')
+        loadData()
+      } else {
+        message.error('请填写完整信息')
+      }
+    })
+  }
+
+  function onExpandedKeys(keys: number[]) {
+    expandedKeys.value = keys
+  }
+
+  function packHandle() {
+    if (expandedKeys.value.length) {
+      expandedKeys.value = []
+    } else {
+      expandedKeys.value = unref(treeData).map((item: IGoodsCategoryTreeItem) => item.id!)
+    }
+  }
+
+  onMounted(() => {
+    loadData()
+  })
+
+  async function loadData() {
+    const { list } = await queryGoodsCategoryTree()
+    const keys = list.map((item: IGoodsCategoryTreeItem) => item.id)
+    Object.assign(state.formData, keys)
+    treeData.value = list
+    loading.value = false
+  }
+</script>
